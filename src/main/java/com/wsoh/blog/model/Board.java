11672529cd509aa7e0ab7246dd3faf8098081789
wooseokoh <allsafe1206@gmail.com>
2021-03-15 00:31:56 +0900

    JPA ORM 연관관계 설정

--- src/main/java/com/wsoh/blog/model/Board.java
+++ src/main/java/com/wsoh/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.wsoh.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,12 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 
-	@ManyToOne // Many = Board, User = One 사용자는 여려개의 개시글을 쓸수 있다.
+	@ManyToOne(fetch = FetchType.EAGER) // Many = Board, User = One 사용자는 여려개의 개시글을 쓸수 있다.
 	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK,자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 준인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	private List<Reply> reply; 
 	
 	@CreationTimestamp // 시간이 자동 입력
 	private Timestamp createDate;
